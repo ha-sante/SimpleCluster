e04d987ac91ab72f2912d9ba12b72ff28e15b762
Henry Asante <90225652+ha-sante@users.noreply.github.com>
2022-11-15 11:14:32 +0000

    Init

--- Tonic/src/server.rs
+++ Tonic/src/server.rs
@@ -1,7 +1,11 @@
-use std::net::IpAddr;
+use port_scanner::*;
+use std::net::*;
 use winping::{Buffer, Pinger};
 
+use std::net::Ipv6Addr;
+use std::str::FromStr;
 use std::thread;
+
 use tonic::{transport::Server, Request, Response, Status};
 
 use dondon::instance_client::InstanceClient;
@@ -31,20 +35,19 @@ impl Instance for DondonInstance {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let find = find_port();
-    let port = find.to_string();
-    let addr = format!("[::1]:{}", port).parse().unwrap();
+    let port = find_port();
     let greeter = DondonInstance::default();
+    let socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), port);
 
     let instance_thread = thread::spawn(move || async move {
-        println!("1. Service Instance listening on {}", addr);
+        println!("1. Service Instance listening on {}", socket);
         Server::builder()
             .add_service(InstanceServer::new(greeter))
-            .serve(addr)
+            .serve(socket)
             .await;
     });
 
-    // find_friends(find).await;
+    find_friends(port).await;
     instance_thread.join().unwrap().await;
 
     println!("Logging after the call went through");
@@ -55,63 +58,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 // Scans a port in specific range and returns first open match only
 fn find_port() -> u16 {
     println!("2. Finding a port for this instance");
-    let mut port = 0;
-    // let mut ping = Ping::new();
+    let mut port = 2;
 
-    // for index in 2..40 {
-    //     try!(ping.add_host(format!("::1:{}", index).as_str()));  // IPv4 / IPv6 addresses OK
-    // }
-
-    // let responses = try!(ping.send());
-    // for resp in responses {
-    //     if resp.dropped > 0 {
-    //         println!("No response from host: {}", resp.hostname);
-    //     } else {
-    //         println!("Response from host {} (address {}): latency {} ms",
-    //             resp.hostname, resp.address, resp.latency_ms);
-    //         println!("    all details: {:?}", resp);
-    //     }
-    // }
-
-    // let (pinger, results) = match Pinger::new(None, Some(56)) {
-    //     Ok((pinger, results)) => (pinger, results),
-    //     Err(e) => panic!("Error creating pinger: {}", e),
-    // };
-
-    // pinger.add_ipaddr("8.8.8.8");
-    // pinger.add_ipaddr("1.1.1.1");
-    // pinger.add_ipaddr("7.7.7.7");
-    // pinger.add_ipaddr("2001:4860:4860::8888");
-    // pinger.run_pinger();
-    // loop {
-    //     match results.recv() {
-    //         Ok(result) => match result {
-    //             Idle { addr } => {
-    //                 println!("Idle Address {}.", addr);
-    //             }
-    //             Receive { addr, rtt } => {
-    //                 println!("Receive from Address {} in {:?}.", addr, rtt);
-    //             }
-    //         },
-    //         Err(_) => panic!("Worker threads disconnected before the solution was found!"),
-    //     }
-    // }
-
-    let pinger = Pinger::new().unwrap();
-    let mut buffer = Buffer::new();
-
-    for index in 0..4 {
-        let dst = std::env::args()
-            .nth(1)
-            .unwrap_or(String::from(format!("::1:{}", index)))
-            .parse::<IpAddr>()
-            .expect("Could not parse IP Address");
-
-        println!("Sending Ping to {}.", dst);
-
-        match pinger.send(dst, &mut buffer) {
-            Ok(rtt) => println!("Response time {} ms.", rtt),
-            Err(err) => println!("{}.", err),
+    for index in 0..10 {
+        println!(
+            "- Port {index} open? {}",
+            scan_port_addr(format!("127.0.0.1:{}", index))
+        );
+        let port_in_use = scan_port_addr(format!("127.0.0.1:{}", index));
+        if !port_in_use {
+            port = index;
+            break;
         }
     }
 
@@ -123,27 +80,29 @@ async fn find_friends(skip: u16) -> Result<(), Box<dyn std::error::Error>> {
     println!("3. Finding for other dondons");
     let mut others: Vec<u16> = Vec::new();
     for index in 2..7 {
-        println!("- Currently at index {index}");
-        // println!("- Local port {index} free ? = {}", is_free(index));
-
-        // Call a hello rpc call, if true, then it's him/her
-        // let address = format!("http://[::1]:{}", index);
-        // let mut client = InstanceClient::connect(address).await?;
-        // let request = tonic::Request::new(HelloRequest {
-        //     name: "Tonic".into(),
-        // });
-
-        // let response = client.hello(request).await;
-
-        // println!("RESPONSE={:?}", response);
-
-        // if !is_local_port_free(index) {
-
-        // } else {
-        //     // println!("- Local port {index} is not free");
-        // }
+        // If port is in use
+        let port_in_use = scan_port_addr(format!("127.0.0.1:{}", index));
+        if port_in_use && index != skip {
+            others.push(index);
+        }
     }
-    println!("- Found list of others");
+
+    println!("4. Validating their dondon");
+    let mut valid: Vec<u16> = Vec::new();
+    for port in others {
+        println!("- Validing 127.0.0.1:{} for dondoness", port);
+        let mut client = InstanceClient::connect(format!("http://127.0.0.1:{}", port)).await?;
+
+        let request = tonic::Request::new(HelloRequest {
+            name: "Tonic".into(),
+        });
+
+        let response = client.hello(request).await?;
+
+        println!("RESPONSE={:?}", response);
+        valid.push(port);
+    }
+
     Ok(())
 }
 
